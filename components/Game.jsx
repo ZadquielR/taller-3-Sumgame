8f614e4da825feb46ab1192e110ab791835ff8d9
David Vargas <david.vargas.guillen@ucreativa.com>
2022-04-28 17:41:39 -0600

    taller-3-sumgame

---
+++ components/Game.jsx
@@ -0,0 +1,90 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState, useRef } from "react";
+import Number from "./Number";
+
+export default Game = ({ randomNumbersCount, initialSeconds }) => {
+  const [ randomNumbers, setRandomNumbers ] = useState([]);
+  const [ target, setTarget ] = useState();
+  const [ selectedNumbers, setSelectedNumbers ] = useState([]);
+  const [ remainingSeconds, setRemainingSeconds ] = useState(initialSeconds);
+  const [ gameStatus, setGameStatus ] = useState('PLAYING');
+
+  const intervalId = useRef();
+
+  useEffect(() => console.log(selectedNumbers), [selectedNumbers]);
+
+  useEffect(() => {
+    const numbers = Array.from({ length: randomNumbersCount}).map(() => 1 + Math.floor(10 * Math.random()));
+    const target = numbers.slice(0, randomNumbersCount -2).reduce( (acc, cur) => acc + cur, 0 );
+
+    setRandomNumbers(numbers);
+    setTarget(target);
+
+    intervalId.current = setInterval(() => setRemainingSeconds(seconds => seconds -1), 1000);
+    return () => clearInterval(intervalId.current);
+  }, []);
+
+  useEffect(() => {
+    setGameStatus(() =>  getGameStatus());
+    if (remainingSeconds  === 0 || gameStatus !== 'PLAYING') {
+      clearInterval(intervalId.current);
+    }
+  }, [remainingSeconds, selectedNumbers]);
+
+  const isNumberSelected = numberIndex => selectedNumbers.some(number => number === numberIndex);
+  const selectNumber = number => {
+    setSelectedNumbers([...selectedNumbers, number]);
+  };
+  const getGameStatus = () => {
+    const sumSelected = selectedNumbers.reduce((acc, cur) => acc + randomNumbers[cur], 0);
+    if (remainingSeconds === 0 || sumSelected > target) {
+      return 'LOST';
+    } else if (sumSelected === target) {
+      return 'WON';
+    } else {
+      return 'PLAYING';
+    }
+  };
+
+  return (
+    <View>
+      <Text style={styles.target}>{target}</Text>
+      <Text style={[ styles.target, styles[gameStatus] ]}>{gameStatus}</Text>
+      <Text>{remainingSeconds}</Text>
+      <View style={styles.randomContainer}>
+        {randomNumbers.map((number, index) => (
+          <Number
+            key={index}
+            id={index}
+            number={number}
+            isSelected={isNumberSelected(index) || gameStatus !== 'PLAYING'}
+            onSelected={selectNumber}
+          />
+        ))}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  target: {
+    fontSize: 40,
+    backgroundColor: '#aaa',
+    textAlign: 'center',
+  },
+  randomContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  PLAYING: {
+    backgroundColor: '#bbb'
+  },
+  LOST: {
+    backgroundColor: 'red'
+  },
+  WON: {
+    backgroundColor: 'green'
+  },
+});
